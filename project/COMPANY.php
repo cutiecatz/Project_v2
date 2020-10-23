97df8776e28b749db9748fcf71cb040cc49279fc
pm <73326070+inthegoodoldday@users.noreply.github.com>
2020-10-23 22:43:19 +0700

    Update COMPANY.php

--- project/COMPANY.php
+++ project/COMPANY.php
@@ -9,7 +9,7 @@ $result = mysqli_query($conn,$userQuery);
 <html>
     <head> 
       <meta charset="utf-8">
-      <link rel="stylesheet" type="text/css" href="../project/css/msdee.css">
+      <link rel="stylesheet" type="text/css" href="http://localhost/project_v2/project/css/msdee.css">
       <script src="https://kit.fontawesome.com/a076d05399.js"></script>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
