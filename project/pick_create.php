b42e3ec7f8058ba5f1a9f6c32b82faef0abcdb30
pm <73326070+inthegoodoldday@users.noreply.github.com>
2020-10-23 21:51:20 +0700

    Update pick_create.php

--- project/pick_create.php
+++ project/pick_create.php
@@ -1,6 +1,5 @@
 <?php
 require("server.php");
-require("navbar.php");
 $userQuery = "SELECT * FROM `sale order`";
 $result = mysqli_query($conn,$userQuery);
 $userQuery2 = "SELECT * FROM `storage`";
@@ -9,41 +8,33 @@ $result2 = mysqli_query($conn,$userQuery2);
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../project/css/msdee.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/project_v2/project/css/style_v2.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<h2> PICKING</h2>
-<form action="pick_add.php" method="POST" style="border:1px solid gray;">
-<table>
-<tr>
-    <td>PICKING ID</td>
-    <td><input type="text" name="id"/></td>
-</tr>
-<tr>
-    <td>SALEORDER ID</td>
-    <td><select name="sid" id="sid">
+<h1 class="head">PICKING DOCUMENT</h1>
+<div class="container">
+  <form action="pick_add.php">
+
+    <label for="id">PICKING ID</label>
+    <input type="text" name="pick_id" placeholder="your id...">
+    
+    <label for="id">SALEORDER ID</label>
+    <select name="sid" id="sid">
     <?php while ($row = mysqli_fetch_assoc($result)) { ?>
         <option value="<?php echo $row['sale_id']; ?>"> SO#<?php echo $row['sale_id'];?> </option>
-     <?php } ?>
-    </td>
-</tr>
-<tr>
-    <td>Storage Location</td>
-    <td><select name="st" id="st">
+     <?php } ?> </select>
+     
+    <label for="st">Storage Location</label>
+    <select name="st" id="st">
     <?php while ($row = mysqli_fetch_assoc($result2)) { ?>
         <option value="<?php echo $row['storage_id']; ?>"> <?php echo $row['storage_name'];?> </option>
-     <?php } ?>
-    </td>
-</tr>
-<tr>
-    <td>Pick Date </td>
-    <td><input type="date" name="pick_date"></td>
-</tr>
-
-
-<tr>
+     <?php } ?> </select>
+    
+    <label for="pd">Pick Date </label><br><br>
+    <input type="date" name="pick_date">
+    <br><br>
     <td><input type="submit" value="submit"/></td>
     <td><input type="reset" value="reset"/></td>
 </tr>
