913f6984acd83ee0ac755b555bf16974cb21bb45
pm <73326070+inthegoodoldday@users.noreply.github.com>
2020-10-23 16:17:23 +0700

    Update vendor_create.php

--- project/vendor_create.php
+++ project/vendor_create.php
@@ -1,51 +1,42 @@
 <?php
 require("server.php");
-require("navbar.php");
 ?>
 
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../project/css/msdee.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/project_v2/project/css/style_v2.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<h2>Add Customer</h2>
-<form action="vendor_add.php" method="POST" style="border:1px solid gray;">
-<table>
-<tr>
-    <td>VendorID </td>
-    <td><input type="text" name="id"></td>
-</tr>
-<tr>
-    <td>Vendor Name </td>
-    <td><input type="text" name="name"></td>
-</tr>
-<tr>
-    <td>Address</td>
-    <td><input type="text" name="add"></td>
-</tr>
-<tr>
-    <td>City</td>
-    <td><input type="text" name="city"></td>
-</tr>
-<tr>
-    <td>Post Code</td>
-    <td><input type="text" name="post"></td>
-</tr>
-<tr>
-    <td>Telephone</td>
-    <td><input type="text" name="phone"></td>
-</tr>
-<tr>
-    <td>E-mail</td>
-    <td><input type="email" name="mail"></td>
-</tr>
-<tr>
+<h1 class="head">Add Customer</h1>
+<div class="container">
+  <form action="vendor_add.php">
+ 
+    <label for="id">Vendor ID</label>
+    <input type="text" name="id" placeholder="your vendor id...">
+
+    <label for="vname">Vendor Name</label>
+    <input type="text" name="id" placeholder="your vendor name...">
+
+    <label for="add">Address</label>
+    <input type="text" name="add" placeholder="your address...">
+
+    <label for="city">City</label>
+    <input type="text" name="city" placeholder="your city...">
+
+    <label for="pcode">Post Code</label>
+    <input type="text" name="post" placeholder="your post code...">
+
+    <label for="phone">Telephone</label>
+    <input type="text" name="post" placeholder="your phone number..">
+
+    <label for="email">E-mail</label>
+    <input type="text" name="email" placeholder="your e-mail...">
+
     <td><input type="submit" value="submit"/></td>
     <td><input type="reset" value="reset"/></td>
-</tr>
-</table>
+
 </form>
 </html>
