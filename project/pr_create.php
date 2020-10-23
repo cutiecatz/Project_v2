01e07cf31e2cf6692638aa4062024717dc426109
pm <73326070+inthegoodoldday@users.noreply.github.com>
2020-10-23 16:27:58 +0700

    Update pr_create.php

--- project/pr_create.php
+++ project/pr_create.php
@@ -1,6 +1,5 @@
 <?php
 require("server.php");
-require("navbar.php");
 $userQuery = "SELECT * FROM customer";
 $result = mysqli_query($conn,$userQuery);
 $userQuery2 = "SELECT * FROM company";
@@ -11,32 +10,29 @@ $c=1;
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../project/css/msdee.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/project_v2/project/css/style_v2.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
-<h2>Add Purchase Requisition</h2>
-<form action="pr_add.php" method="POST" style="border:1px solid gray;">
-<table>
-<tr>
-    <td>Purchase Requisition ID</td>
-    <td><input type="text" name="pr_id"/></td>
-</tr>
-<td>Company Code</td>
-    <td><select name="com" id="com">
+<h1 class="head">Add Purchase Requisition</h1>
+<div class="container">
+  <form action="pr_add.php">
+
+    <label for="id">Purchase Requisition ID</label>
+    <input type="text" name="pr_id" placeholder="your id...">
+    
+    <label for="ccode">Company Code</label>
+    <select name="com" id="com">
     <?php while ($row = mysqli_fetch_assoc($result2)) { ?>
         <option value="<?php $i=1; echo "$c"; ?>"> <?php echo $row['company_code']; $c+=$c?> </option>
      <?php } ?>
-    </td>
-<tr>
-    <td>Purchase Requisition Date </td>
-    <td><input type="date" name="date"></td>
-</tr>
-<tr>
+    
+    <label for="id">Purchase Requisition Date</label>
+    <input type="text" name="date" placeholder="your date...">
+   
     <td><input type="submit" value="submit"/></td>
     <td><input type="reset" value="reset"/></td>
-</tr>
-</table>
+
 </form>
 </html>
