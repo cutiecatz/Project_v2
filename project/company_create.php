5aaf65ea93424234774014e532745fa103f6813c
pm <73326070+inthegoodoldday@users.noreply.github.com>
2020-10-23 15:44:21 +0700

    Update company_create.php

--- project/company_create.php
+++ project/company_create.php
@@ -1,44 +1,56 @@
 <?php
 require("server.php");
-
+require("navbar.php");
 ?>
 
 <html>
 <head>
+<<<<<<< HEAD
 <link rel="stylesheet" type="text/css" href="http://localhost/project_v2/project/css/style_v2.css">
+=======
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../project/css/msdee.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+>>>>>>> 23d0aab2a5df269d03e69b2b412a8bac53a284e5
 </head>
 
-<h1 class="head">Add Customer</h1>
-<div class="container">
-  <form action="company_add.php">
-
-    <label for="id">ID</label>
-    <input type="text" name="company_id" placeholder="your id...">
-
-    <label for="cname">Company Name</label>
-    <input type="text" name="company_name" placeholder="your company name...">
-
-    <label for="code">Company Code</label>
-    <input type="text" name="company_code" placeholder="your company code...">
-
-    <label for="add">Address</label>
-    <input type="text" name="company_add" placeholder="your company address...">
-
-    <label for="city">City</label>
-    <input type="text" name="company_city" placeholder="your company city...">
-
-    <label for="pcode">Post Code</label>
-    <input type="text" name="company_post" placeholder="your company post code...">
-
-    <label for="phone">Telephone </label>
-    <input type="text" name="company_phone" placeholder="your company telephone...">
-
-    <label for="mail">E-mail </label>
-    <input type="text" name="email" placeholder="your company email...">
-
-
-
-
+<h2>Add Customer</h2>
+<form action="company_add.php" method="POST" style="border:1px solid gray;">
+<table>
+<tr>
+    <td>ID </td>
+    <td><input type="text" name="company_id"></td>
+</tr>
+<tr>
+    <td>Company Name </td>
+    <td><input type="text" name="company_name"></td>
+</tr>
+<tr>
+    <td>Company Code </td>
+    <td><input type="text" name="company_code"></td>
+</tr>
+<tr>
+    <td>Address</td>
+    <td><input type="text" name="company_add"></td>
+</tr>
+<tr>
+    <td>City</td>
+    <td><input type="text" name="company_city"></td>
+</tr>
+<tr>
+    <td>Post Code </td>
+    <td><input type="text" name="company_post"></td>
+</tr>
+<tr>
+    <td>Telephone </td>
+    <td><input type="text" name="company_phone"></td>
+</tr>
+<tr>
+    <td>E-mail</td>
+    <td><input type="email" name="company_mail"></td>
+</tr>
+<tr>
     <td><input type="submit" value="submit"/></td>
     <td><input type="reset" value="reset"/></td>
 </tr>
