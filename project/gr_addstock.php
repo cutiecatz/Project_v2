597bbec55c5f2d0f6b3453ac3785963f0cbbb1db
namchanathip <68407703+namchanathip@users.noreply.github.com>
2020-10-27 02:37:06 +0700

    Update gr_addstock.php

--- project/gr_addstock.php
+++ project/gr_addstock.php
@@ -15,23 +15,24 @@ $result = mysqli_query($conn,$userQuery);
 while ($row = mysqli_fetch_assoc($result)) { 
     $qty[$i] = $row['qty'];
     $proid[$i] = $row['product_id'];
+    $sto = $row['storage_id'];
     $query = "INSERT INTO `gr detail`(`gr_id`, `product_id`, `qty`) VALUES ('$id','$proid[$i]','$qty[$i]')";
     $result3 = mysqli_query($conn,$query);
-    $Query = "SELECT * FROM cutstock JOIN `storage detail` USING (storage_id,`product_id`) WHERE product_id = $proid[$i]";
+    $Query = "SELECT * FROM `goods receipt` JOIN `gr detail` USING (gr_id) JOIN `storage detail` USING (storage_id) WHERE gr_id = $id";
     $res = mysqli_query($conn,$Query);
     while ($row2 = mysqli_fetch_assoc($res)){
         $stock = $row2['product_qty'];
-        $cut  = $row2['cut_qty'];
+        $cut  = $row2['qty'];
         $pdid  = $row2['product_id'];
         $stoid  = $row2['storage_id'];
-        $total  = $stock - $cut ;
+        $total  = $stock + $cut ;
         $query2  = "UPDATE `storage detail` SET product_qty = $total WHERE product_id = '$pdid' AND storage_id = '$stoid'";
         $result2 = mysqli_query($conn,$query2);
         $d++;
     }
 $i++;
 }
-header("location: gr.php")
+header("location: gr.php");
 
 
 ?>
