e4cee4576e7ec6b802c577d25a7b0951646ba80a
nixe <68407676+leonasnice@users.noreply.github.com>
2020-10-26 20:53:01 +0700

    Update quo_doc.php

--- project/quo_doc.php
+++ project/quo_doc.php
@@ -4,7 +4,7 @@ $quo_id = $_GET['id'];
 $userQuery = "SELECT * FROM `quotation` 
                        JOIN vendor  USING (vendor_id)
                        JOIN company  USING (company_id)
-                       JOIN rfq  USING (rfqr_id)
+                       JOIN rfq  USING (rfq_id)
                        WHERE quo_id = $quo_id";
 $result = mysqli_query($conn,$userQuery);
 $userQuery2 = "SELECT * FROM `quo detail` p inner join product d ON p.product_id = d.product_id where quo_id = '$quo_id'";
