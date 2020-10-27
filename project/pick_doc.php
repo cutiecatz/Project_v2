7ef2841844c60b1e0123a898116c11b6b314f064
Noppawit First <44127891+cutiecatz@users.noreply.github.com>
2020-10-28 04:18:59 +0700

    Update pick_doc.php

--- project/pick_doc.php
+++ project/pick_doc.php
@@ -20,80 +20,98 @@ $result2 = mysqli_query($conn,$userQuery2);
 <!doctype html>
 <html>
 <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../project/css/doc.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"rel="stylesheet"/>
+
 </head>
 <body>
-    <div class="middle">
+    <div class="container">
         <div class="document">
-            <header>
-                    <div class="header">
-                        <h1>Picking Document</h1>
+            <div>
+                <h1>Picking Document</h1>
+            </div>
+                <div class="inv-header">
                         Picking Document#: <?php echo $pick_id;?><br>
+                        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                        Picking DATE :  <?php echo "".$row['pick_date']."" ?><br>
+                        <!--------------- ------------->
+                    <div class="company_to">
+                        <div>
+                            <h2><p class="Address-Heading_from">From : </p></h2>
+                            <ul>
+                                <li>Name:  <?php echo "".$row['company_name']."" ?></li> 
+                                <li>Address: <?php echo "".$row['company_address']."" ?>
+                                <?php echo "".$row['company_city']." ".$row['company_post']." ".$row['company_country']."" ?></li> 
+                                <li>E-mail: <?php echo "".$row['company_email']."" ?></li> 
+                                <li>Phone : <?php echo "0".$row['company_phone']."" ?></li> 
+                            </ul>
+                        </div>
                     </div>
-            </header>
-                <div class="date">
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                            Picking DATE :  <?php echo "".$row['pick_date']."" ?><br>
-                </div>
                     <!--------------- ------------->
-                <div class="company_to">
+                    <div class="customer_from">
                         <div>
-                            <p class="Address-Heading_from">From : </p>
-                            Name:  <?php echo "".$row['company_name']."" ?><br>
-                            Address: <?php echo "".$row['company_address']."" ?><br>
-                            <?php echo "".$row['company_city']." ".$row['company_post']." ".$row['company_country']."" ?><br>
-                            E-mail: <?php echo "".$row['company_email']."" ?><br>
-                            Phone : <?php echo "0".$row['company_phone']."" ?><br>
+                            <h2><p class="Address-Heading_from">To : </p></h2>
+                            <ul>
+                                <li>Name:  <?php echo "".$row['company_name']."" ?></li>
+                                <li>Address: <?php echo "".$row['company_address']."" ?>
+                                <?php echo "".$row['company_city']." ".$row['company_post']." ".$row['company_country']."" ?></li>
+                                <li>E-mail: <?php echo "".$row['company_email']."" ?></li>
+                                <li>Phone : <?php echo "0".$row['company_phone']."" ?></li>
+                            </ul>
                         </div>
+                    </div>
                 </div>
-                <div class="customer_from">
-                        <div>
-                            <p class="Address-Heading_from">To : </p>
-                            Name:  <?php echo "".$row['company_name']."" ?><br>
-                            Address: <?php echo "".$row['company_address']."" ?><br>
-                            <?php echo "".$row['company_city']." ".$row['company_post']." ".$row['company_country']."" ?><br>
-                            E-mail: <?php echo "".$row['company_email']."" ?><br>
-                            Phone : <?php echo "0".$row['company_phone']."" ?><br>
-                        </div>
-                </div>
-                <div class="content">
-                <table class="info">
-                        <tr>
-                            <th>Order#</th>
-                            <th>S.O.Date</th>
-                            <th>Shipped VIA</th>
-                            <th>Delivery Date</th>
-                            <th>F.O.B. Point </th>
-                            <th>TERMS </th>
-                        </tr>
-                        <tr class="order_info">
-                            <?php echo "<td>".$row['sale_id']."</td>" ?>
-                            <?php echo "<td>".$row['sale_date']."</td>" ?>
-                            <?php echo "<td>".$row['ship_method']."</td>" ?>
-                            <?php echo "<td>".$row['sale_due_date']."</td>" ?>
-                            <?php echo "<td> Destination </td>" ?>
-                            <?php echo "<td> Net 30 </td>" ?>
-                        <?php } ?>
-                        </tr>
-                </table>
-                <table class="stock">
-                        <tr>
+                <!--------------- ------------->
+                <div class="inv-body">
+                    <table class="info">
+                            <thead>
+                                <th>Order#</th>
+                                <th>S.O.Date</th>
+                                <th>Shipped VIA</th>
+                                <th>Delivery Date</th>
+                                <th>F.O.B. Point </th>
+                                <th>TERMS </th>
+                            </thead>
+                            <tbody class="order_info">
+                                <tr>
+                                    <?php echo "<td>".$row['sale_id']."</td>" ?>
+                                    <?php echo "<td>".$row['sale_date']."</td>" ?>
+                                    <?php echo "<td>".$row['ship_method']."</td>" ?>
+                                    <?php echo "<td>".$row['sale_due_date']."</td>" ?>
+                                    <?php echo "<td> Destination </td>" ?>
+                                    <?php echo "<td> Net 30 </td>" ?>
+                                    <?php } ?>
+                                </tr>
+                            </tbody>
+                    </table>
+                    <!--------------- ------------->
+                    <table class="stock">
+                        <thead>
                             <th>Product#</th>
                             <th>Product Description</th>
                             <th>Quantity Ordered</th>
                             <th>Storage Location</th>
                             <th>Quantity Packed</th>
                             <th>Storage Location</th>
-                        </tr>
-                <?php while ($row = mysqli_fetch_assoc($result2)) { ?>
-                        <tr class="order_info">
-                            <?php echo "<td>".$row['product_id']."</td>" ?>
-                            <?php echo "<td>".$row['product_descrip']."</td>" ?>
-                            <?php echo "<td>".$row['cut_qty']."</td>" ?>
-                            <?php echo "<td>".$row['product_qty']."</td>" ?>
-                            <?php echo "<td>  </td>" ?>
-                            <?php echo "<td>  </td>" ?>
-                            <?php } ?>
-                        </tr>
-                </table>
+                        </thead>
+                        <?php while ($row = mysqli_fetch_assoc($result2)) { ?>
+                        <tbody>
+                            <tr class="order_info">
+                                <?php echo "<td>".$row['product_id']."</td>" ?>
+                                <?php echo "<td>".$row['product_descrip']."</td>" ?>
+                                <?php echo "<td>".$row['cut_qty']."</td>" ?>
+                                <?php echo "<td>".$row['product_qty']."</td>" ?>
+                                <?php echo "<td>  </td>" ?>
+                                <?php echo "<td>  </td>" ?>
+                                <?php } ?>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+            </div>
+        </div>
+    </div>
 </body>
