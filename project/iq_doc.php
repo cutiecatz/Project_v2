c7339cfb3d7f70ba9c35bc1294b4e92e56b23a3a
Noppawit First <44127891+cutiecatz@users.noreply.github.com>
2020-10-28 03:17:29 +0700

    Update iq_doc.php

--- project/iq_doc.php
+++ project/iq_doc.php
@@ -11,49 +11,66 @@ $result2 = mysqli_query($conn,$userQuery2);
 <!doctype html>
 <html>
 <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../project/css/doc.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"rel="stylesheet"/>
 </head>
 <body>
-    <div class="middle">
+    <div class="container">
         <div class="document">
-            <header>
-                    <div class="header">
-                        <h1>Inquiry</h1>
-                        Inquiry#: <?php echo $inquiry_id;?><br>
-                    </div>
-                </header>
-                <div class="date">
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                            DATE :  <?php echo "".$row['inquiry_date']."" ?><br>
-                            Due DATE :  <?php echo "".$row['inquiry_due_date']."" ?><br>
-
-                    </div>
-                    <!--------------- ------------->
-                <div class="customer">
+                <div class="inv-title">
+                    <h1>Inquiry</h1>
+                </div>
+                <!--------------- ------------->
+                <div class = "inv-header">
+                    Inquiry#: <?php echo $inquiry_id;?><br>
+                    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                        
+                    <div class="customer">
                         <div>
-                            <p class="Address-Heading">From : </p>
-                            Customer Name:  <?php echo "".$row['customer_name']."" ?><br>
-                            Address: <?php echo "".$row['customer_bill']."" ?><br>
-                            <?php echo "".$row['customer_bill_city']." ".$row['customer_bill_zipcode']."" ?><br>
-                            E-mail: <?php echo "".$row['customer_email']."" ?><br>
-                            Phone : <?php echo "".$row['customer_phone']."" ?><br>
-                            <?php }?>
+                            <h2><p class="Address-Heading">From : </p></h2>
+                            <ul>
+                                <li>Customer Name:  <?php echo "".$row['customer_name']."" ?></li>
+                                <li>Address: <?php echo "".$row['customer_bill']."" ?>
+                                    <?php echo "".$row['customer_bill_city']." ".$row['customer_bill_zipcode']."" ?></li>
+                                <li>E-mail: <?php echo "".$row['customer_email']."" ?></li>
+                                <li>Phone : <?php echo "".$row['customer_phone']."" ?></li>
+                            </ul>
                         </div>
                     </div>
+                    <div>
+                        <table>
+                            <tr>
+                                <th> DATE : </th> 
+                                <td><?php echo "".$row['inquiry_date']."" ?><br></td>
+                            </tr>
+                            <tr>
+                                <th> Due DATE : </th> 
+                                <td> <?php echo "".$row['inquiry_due_date']."" ?></td>
+                            </tr>
+                            <?php }?>
+                        </table>
+                    </div>
+                </div>    
                     <!--------------- ------------->
-                <div class="content">
+                <div class="inv-body">
                 <table class="product">
-                        <tr>
+                        <thead>
                             <th>Product#</th>
                             <th>Product Name</th>
                             <th>Quantity</th>
-                        </tr>
+                        </thead>
                         <?php while ($row = mysqli_fetch_assoc($result2)) { ?> 
+                        <tbody>
                             <tr class="item">
                                 <?php echo "<td>".$row['product_id']."</td>" ?>
                                 <?php echo "<td>".$row['product_descrip']."</td>" ?>
                                 <?php echo "<td>".$row['product_qty']."</td>" ?>
                             </tr>
                         <?php } ?>
+                        </tbody>
                     </table> 
                 </div>
         </div>
